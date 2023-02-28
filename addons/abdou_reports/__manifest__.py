87b2c966baa27d73a6aab23d9985e1219f22c515
Abdoumo <bedoushop@gmail.com>
2023-02-28 17:51:48 +0100

    here

---
+++ addons/abdou_reports/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Reports',
+    'license': 'LGPL-3',
+    'version': '1.0',
+    'category': 'Inventory',
+    'summary': 'Custom inventory product report',
+    'description': '',
+    'author': 'Aboud le blue',
+    'depends': ['stock'],
+'data': [
+    'reports/inventory_report.xml',
+    'reports/inventory_products_form.xml',
+],
+    'application': True,
+}
